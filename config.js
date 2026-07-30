1c16eece225f678231457e95fa6a766e8b08c801
jiqi8716 <158222796+jiqi8716@users.noreply.github.com>
2026-07-30 22:34:07 +0800

    Add Supabase configuration

---
+++ config.js
@@ -0,0 +1,4 @@
+window.WORKBENCH_CONFIG = Object.freeze({
+  supabaseUrl: 'https://rsqwrrsjnxuhakwdxajn.supabase.co',
+  supabasePublishableKey: 'sb_publishable_eC3uNW2EThTfjG-5BSiPJQ_anOin86o'
+});
